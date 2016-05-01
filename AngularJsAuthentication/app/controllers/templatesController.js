418eaea492642e8dd8bd5e418123c701c289ac8d
Patrick Armentrout <patrick.armentrout@gmail.com>
2016-05-01 16:48:58 -0600

    Template stuff

--- AngularJsAuthentication/app/controllers/templatesController.js
+++ AngularJsAuthentication/app/controllers/templatesController.js
@@ -2,4 +2,23 @@
 app.controller('templatesController', [
         '$scope', function ($scope) {
             $scope.hello = "Hello World";
-        }]);
+            scope.templates = [
+                {name: "Paycheck 1"},
+                { name: "Paycheck 2" },
+                { name: "Paycheck 3" }
+            ];
+
+            $scope.categories = [
+               { name: 'To Savings' },
+               { name: 'Bills' },
+               { name: 'Groceries' },
+               { name: 'Gas' },
+               { name: 'Coffee' },
+               { name: 'Baby' },
+               { name: 'Misc' },
+               { name: 'Home' },
+               { name: 'Restaurants' },
+               { name: 'Charity' },
+               { name: 'Clothing/Wellness' }
+            ];
+}]);
